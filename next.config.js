94261ca99a0cdd9905909654075dff9a77f173c8
Borislav Bojkov <borislavbojkov@Bobs-MacBook-AirM1.local>
2025-12-17 01:05:51 -0300

    Replace next.config.ts with JS config (CloudLinux fix)

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // ако ползваш /dev/bridge:
+    basePath: '/dev/bridge',
+    assetPrefix: '/dev/bridge',
+  
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
